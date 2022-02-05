55a4d436ebe681800c38a12a32eb2c78b7c6bef9
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-02-05 10:59:21 +0900

    Initial Commit

---
+++ Shared/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
